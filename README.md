5adbe3447dcd39b172eb2efed162152cf5f06883
Shini Ko <ko.shmily@gmail.com>
2018-09-27 16:38:42 -0400

    brightness and contrast, and scaling done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # image-editor
+Student 1:
+Name: Shini Ko
+Student#: 10153214
+NetID: 14sk22
+
+Student 2:
+Name:
+Student#:
+NetID:
--- main.py
+++ main.py
@@ -15,48 +15,42 @@
 
 import sys, os, numpy, math
 
-try: # Pillow
-  from PIL import Image
+try:  # Pillow
+    from PIL import Image
 except:
-  print 'Error: Pillow has not been installed.'
-  sys.exit(0)
+    print 'Error: Pillow has not been installed.'
+    sys.exit(0)
 
-try: # PyOpenGL
-  from OpenGL.GLUT import *
-  from OpenGL.GL import *
-  from OpenGL.GLU import *
+try:  # PyOpenGL
+    from OpenGL.GLUT import *
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
 except:
-  print 'Error: PyOpenGL has not been installed.'
-  sys.exit(0)
-
-
+    print 'Error: PyOpenGL has not been installed.'
+    sys.exit(0)
 
 # Globals
 
-windowWidth  = 600 # window dimensions
-windowHeight =  800
+windowWidth = 600  # window dimensions
+windowHeight = 800
 
 localHistoRadius = 5  # distance within which to apply local histogram equalization
 
-
-
 # Current image
 
-imgDir      = 'images'
+imgDir = 'images'
 imgFilename = 'mandrill.png'
 
-currentImage = Image.open( os.path.join( imgDir, imgFilename ) ).convert( 'YCbCr' ).transpose( Image.FLIP_TOP_BOTTOM )
-tempImage    = None
-
-
+currentImage = Image.open(os.path.join(imgDir, imgFilename)).convert('YCbCr').transpose(Image.FLIP_TOP_BOTTOM)
+tempImage = None
 
 # File dialog (doesn't work on Mac OSX)
 
 if sys.platform != 'darwin':
-  import Tkinter, tkFileDialog
-  root = Tkinter.Tk()
-  root.withdraw()
+    import Tkinter, tkFileDialog
 
+    root = Tkinter.Tk()
+    root.withdraw()
 
 
 # Apply brightness and contrast to tempImage and store in
@@ -66,124 +60,187 @@ if sys.platform != 'darwin':
 # see the changes immediately.  As long as left mouse button is held
 # down, tempImage will not change.
 
-def applyBrightnessAndContrast( brightness, contrast ):
+def applyBrightnessAndContrast(brightness, contrast):
+    width = currentImage.size[0]
+    height = currentImage.size[1]
 
-  width  = currentImage.size[0]
-  height = currentImage.size[1]
+    srcPixels = tempImage.load()
+    dstPixels = currentImage.load()
 
-  srcPixels = tempImage.load()
-  dstPixels = currentImage.load()
+    # YOUR CODE HERE
+    # applyBrightnessAndContrast(255 * diffX / float(windowWidth), 1 + diffY / float(windowHeight))
+    for x in range(width):
+        for y in range(height):
+            rgb = YCbCr2RGB(srcPixels[x, y])
+            new_r = cut_off_255(rgb[0]*contrast + brightness)
+            new_g = cut_off_255(rgb[1]*contrast + brightness)
+            new_b = cut_off_255(rgb[2]*contrast + brightness)
+            new_rgb = tuple((new_r, new_g, new_b))
+            ycbcr = RGB2YCbCr(new_rgb)
+            dstPixels[x, y] = ycbcr
 
-  # YOUR CODE HERE
+    print 'adjust brightness = %f, contrast = %f' % (brightness, contrast)
 
-  print 'adjust brightness = %f, contrast = %f' % (brightness,contrast)
-
-  
 
 # Perform local histogram equalization on the current image using the given radius.
 
-def performHistoEqualization( radius ):
+def performHistoEqualization(radius):
+    pixels = currentImage.load()
+    width = currentImage.size[0]
+    height = currentImage.size[1]
 
-  pixels = currentImage.load()
-  width  = currentImage.size[0]
-  height = currentImage.size[1]
+    # YOUR CODE HERE
 
-  # YOUR CODE HERE
-
-  print 'perform local histogram equalization with radius %d' % radius
-
+    print 'perform local histogram equalization with radius %d' % radius
 
 
 # Scale the tempImage by the given factor and store it in
 # currentImage.  Use backward projection.  This is called when the
 # mouse is moved with the right button held down.
 
-def scaleImage( factor ):
+def scaleImage(factor):
+    width = currentImage.size[0]
+    height = currentImage.size[1]
 
-  width  = currentImage.size[0]
-  height = currentImage.size[1]
+    srcPixels = tempImage.load()
+    dstPixels = currentImage.load()
+    newWidth = int(width * factor)
+    newHeight = int(height * factor)
+    print("new dimension: " + str(newWidth) + "x" + str(newHeight))
 
-  srcPixels = tempImage.load()
-  dstPixels = currentImage.load()
+    # if we are scaling the image smaller, we fill the remaining pixels with white
+    if factor < 1:
+        for x in range(width):
+            for y in range(newHeight, height):
+                dstPixels[x, y] = (235, 128, 128) # white in YCbCr
+        for y in range(height):
+            for x in range(newWidth, width):
+                dstPixels[x, y] = (235, 128, 128)
 
-  # YOUR CODE HERE
+    for x in range(min(width, newWidth)):
+        for y in range(min(height, newHeight)):
+            srcX = int(x / factor)
+            srcY = int(y / factor)
+            dstPixels[x, y] = srcPixels[srcX, srcY]
 
-  print 'scale image by %f' % factor
+    print 'scale image by %f' % factor
 
-  
+
+def RGB2YCbCr(rgb):
+    r = rgb[0] / float(255)
+    g = rgb[1] / float(255)
+    b = rgb[2] / float(255)
+
+    y = r * 0.299 + g * 0.587 + b * 0.114
+    pb = r * -0.168736 - g * 0.331264 + b * 0.5
+    pr = r * 0.5 - g * 0.418688 - b * 0.081312
+
+    y = int(16 + 219 * y)
+    cb = int(128 + 224 * pb)
+    cr = int(128 + 224 * pr)
+
+    return (y, cb, cr)
+
+
+def YCbCr2RGB(ycrcb):
+    y = ycrcb[0]
+    cb = ycrcb[1]
+    cr = ycrcb[2]
+
+    y = (y - 16) / float(219)
+    pb = (cb - 128) / float(112)
+    pr = (cr - 128) / float(112)
+
+    r = wrap_around(int((y + pr * 0.701) * 255))
+    g = wrap_around(int((y - pb * 0.17206 - pr * 0.35705) * 255))
+    b = wrap_around(int((y + pb * 0.886) * 255))
+    return (r, g, b)
+
+
+def wrap_around(value):
+    if value < 0:
+        return value + 255
+    elif value > 255:
+        return value -255
+    else:
+        return value
+
+def cut_off_255(value):
+    if value > 255:
+        return 255
+    elif value < 0:
+        return 0
+    else:
+        return value
 
 # Set up the display and draw the current image
 
 def display():
+    # Clear window
 
-  # Clear window
+    glClearColor(1, 1, 1, 0)
+    glClear(GL_COLOR_BUFFER_BIT)
 
-  glClearColor ( 1, 1, 1, 0 )
-  glClear( GL_COLOR_BUFFER_BIT )
+    # rebuild the image
 
-  # rebuild the image
+    img = currentImage.convert('RGB')
 
-  img = currentImage.convert( 'RGB' )
+    width = img.size[0]
+    height = img.size[1]
 
-  width  = img.size[0]
-  height = img.size[1]
+    # Find where to position lower-left corner of image
 
-  # Find where to position lower-left corner of image
+    baseX = (windowWidth - width) / 2
+    baseY = (windowHeight - height) / 2
 
-  baseX = (windowWidth-width)/2
-  baseY = (windowHeight-height)/2
+    glWindowPos2i(baseX, baseY)
 
-  glWindowPos2i( baseX, baseY )
+    # Get pixels and draw
 
-  # Get pixels and draw
+    imageData = numpy.array(list(img.getdata()), numpy.uint8)
 
-  imageData = numpy.array( list( img.getdata() ), numpy.uint8 )
+    glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, imageData)
 
-  glDrawPixels( width, height, GL_RGB, GL_UNSIGNED_BYTE, imageData )
-
-  glutSwapBuffers()
+    glutSwapBuffers()
 
 
-  
 # Handle keyboard input
 
-def keyboard( key, x, y ):
+def keyboard(key, x, y):
+    global localHistoRadius
 
-  global localHistoRadius
+    if key == '\033':  # ESC = exit
+        sys.exit(0)
 
-  if key == '\033': # ESC = exit
-    sys.exit(0)
+    elif key == 'l':
+        if sys.platform != 'darwin':
+            path = tkFileDialog.askopenfilename(initialdir=imgDir)
+            if path:
+                loadImage(path)
 
-  elif key == 'l':
-    if sys.platform != 'darwin':
-      path = tkFileDialog.askopenfilename( initialdir = imgDir )
-      if path:
-        loadImage( path )
+    elif key == 's':
+        if sys.platform != 'darwin':
+            outputPath = tkFileDialog.asksaveasfilename(initialdir='.')
+            if outputPath:
+                saveImage(outputPath)
 
-  elif key == 's':
-    if sys.platform != 'darwin':
-      outputPath = tkFileDialog.asksaveasfilename( initialdir = '.' )
-      if outputPath:
-        saveImage( outputPath )
+    elif key == 'h':
+        performHistoEqualization(localHistoRadius)
 
-  elif key == 'h':
-    performHistoEqualization( localHistoRadius )
+    elif key in ['+', '=']:
+        localHistoRadius = localHistoRadius + 1
+        print 'radius =', localHistoRadius
 
-  elif key in ['+','=']:
-    localHistoRadius = localHistoRadius + 1
-    print 'radius =', localHistoRadius
+    elif key in ['-', '_']:
+        localHistoRadius = localHistoRadius - 1
+        if localHistoRadius < 1:
+            localHistoRadius = 1
+        print 'radius =', localHistoRadius
 
-  elif key in ['-','_']:
-    localHistoRadius = localHistoRadius - 1
-    if localHistoRadius < 1:
-      localHistoRadius = 1
-    print 'radius =', localHistoRadius
+    else:
+        print 'key =', key  # DO NOT REMOVE THIS LINE.  It will be used during automated marking.
 
-  else:
-    print 'key =', key    # DO NOT REMOVE THIS LINE.  It will be used during automated marking.
-
-  glutPostRedisplay()
-
+    glutPostRedisplay()
 
 
 # Load and save images.
@@ -194,33 +251,28 @@ def keyboard( key, x, y ):
 # they will be used in automated marking.
 
 
-def loadImage( path ):
+def loadImage(path):
+    global currentImage
 
-  global currentImage
-
-  currentImage = Image.open( path ).convert( 'YCbCr' ).transpose( Image.FLIP_TOP_BOTTOM )
+    currentImage = Image.open(path).convert('YCbCr').transpose(Image.FLIP_TOP_BOTTOM)
 
 
-def saveImage( path ):
+def saveImage(path):
+    global currentImage
 
-  global currentImage
-
-  currentImage.transpose( Image.FLIP_TOP_BOTTOM ).convert('RGB').save( path )
-  
+    currentImage.transpose(Image.FLIP_TOP_BOTTOM).convert('RGB').save(path)
 
 
 # Handle window reshape
 
 
-def reshape( newWidth, newHeight ):
+def reshape(newWidth, newHeight):
+    global windowWidth, windowHeight
 
-  global windowWidth, windowHeight
+    windowWidth = newWidth
+    windowHeight = newHeight
 
-  windowWidth  = newWidth
-  windowHeight = newHeight
-
-  glutPostRedisplay()
-
+    glutPostRedisplay()
 
 
 # Mouse state on initial click
@@ -230,68 +282,63 @@ initX = 0
 initY = 0
 
 
-
 # Handle mouse click/release
 
-def mouse( btn, state, x, y ):
+def mouse(btn, state, x, y):
+    global button, initX, initY, tempImage
 
-  global button, initX, initY, tempImage
+    if state == GLUT_DOWN:
+        tempImage = currentImage.copy()
+        button = btn
+        initX = x
+        initY = y
+    elif state == GLUT_UP:
+        tempImage = None
+        button = None
 
-  if state == GLUT_DOWN:
-    tempImage = currentImage.copy()
-    button = btn
-    initX = x
-    initY = y
-  elif state == GLUT_UP:
-    tempImage = None
-    button = None
+    glutPostRedisplay()
 
-  glutPostRedisplay()
-
-  
 
 # Handle mouse motion
 
-def motion( x, y ):
+def motion(x, y):
+    if button == GLUT_LEFT_BUTTON:
 
-  if button == GLUT_LEFT_BUTTON:
+        diffX = x - initX
+        diffY = y - initY
 
-    diffX = x - initX
-    diffY = y - initY
+        applyBrightnessAndContrast(255 * diffX / float(windowWidth), 1 + diffY / float(windowHeight))
 
-    applyBrightnessAndContrast( 255 * diffX/float(windowWidth), 1 + diffY/float(windowHeight) )
+    elif button == GLUT_RIGHT_BUTTON:
 
-  elif button == GLUT_RIGHT_BUTTON:
+        initPosX = initX - float(windowWidth) / 2.0
+        initPosY = initY - float(windowHeight) / 2.0
+        initDist = math.sqrt(initPosX * initPosX + initPosY * initPosY)
+        if initDist == 0:
+            initDist = 1
 
-    initPosX = initX - float(windowWidth)/2.0
-    initPosY = initY - float(windowHeight)/2.0
-    initDist = math.sqrt( initPosX*initPosX + initPosY*initPosY )
-    if initDist == 0:
-      initDist = 1
+        newPosX = x - float(windowWidth) / 2.0
+        newPosY = y - float(windowHeight) / 2.0
+        newDist = math.sqrt(newPosX * newPosX + newPosY * newPosY)
 
-    newPosX = x - float(windowWidth)/2.0
-    newPosY = y - float(windowHeight)/2.0
-    newDist = math.sqrt( newPosX*newPosX + newPosY*newPosY )
+        scaleImage(newDist / initDist)
 
-    scaleImage( newDist / initDist )
-
-  glutPostRedisplay()
-  
+    glutPostRedisplay()
 
 
 # Run OpenGL
 
 glutInit()
-glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB )
-glutInitWindowSize( windowWidth, windowHeight )
-glutInitWindowPosition( 50, 50 )
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+glutInitWindowSize(windowWidth, windowHeight)
+glutInitWindowPosition(50, 50)
 
-glutCreateWindow( 'imaging' )
+glutCreateWindow('imaging')
 
-glutDisplayFunc( display )
-glutKeyboardFunc( keyboard )
-glutReshapeFunc( reshape )
-glutMouseFunc( mouse )
-glutMotionFunc( motion )
+glutDisplayFunc(display)
+glutKeyboardFunc(keyboard)
+glutReshapeFunc(reshape)
+glutMouseFunc(mouse)
+glutMotionFunc(motion)
 
 glutMainLoop()
